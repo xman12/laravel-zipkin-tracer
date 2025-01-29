db71d804cb1b2e3498130ee8983053ebc22f86f6
Aleksandr Belyshev <xman12@mail.ru>
2025-01-29 17:55:54 +0600

    init project

---
+++ src/Providers/ZipkinTracerProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ZipkinTracer\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ZipkinTracerProvider
+{
+    public function boot()
+    {
+
+    }
+
+    protected function registerConfiguration()
+    {
+
+    }
+
+    protected function registerZipkinTracer()
+    {
+
+    }
+}
